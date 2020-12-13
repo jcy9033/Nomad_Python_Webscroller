9448926b47797bda7d67669af7222d02e10291c3
jcy9033 <jcy9033@gmail.com>
2020-12-13 16:00:29 +0900

    20201213_16:00

--- Python/Webscroller/indeed.py
+++ Python/Webscroller/indeed.py
@@ -23,13 +23,23 @@ def extract_indeed_pages():
 
 def extract_indeed_jobs(last_page):
     jobs = []
-    #for page in range(last_page):
-    #여기 왜 print 가 들어가는가
+    # for page in range(last_page):
+    # 여기 왜 print 가 들어가는가
     result = requests.get(f"{URL}&start={0*LIMIT}")
     soup = BeautifulSoup(result.text, "html.parser")
     results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
     for result in results:
         # title 과 anchor 를 한번에 찾도록 코드 합치기
         title = result.find("h2", {"class": "title"}).find("a")["title"]
-
+        # inspect 에서 url 이 들어간 회사와 그렇지 않은 회사가 나눠져 있어서 if 사용
+        # a(anchor) 는 url 이 걸쳐진 것
+        company = result.find("span", {"class": "company"})
+        company_anchor = company.find("a")
+        if company_anchor is not None:
+            company = str(company_anchor.string)
+        else:
+            company = str(company.string)
+        # 여백(빈 공간) 지우기 위한 코드
+        company = company.strip()
+        print(title, company)
     return jobs
