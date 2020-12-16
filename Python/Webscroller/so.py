e0d1badf38c5e5acde63405b69d468ab45d7b4fa
jcy9033 <jcy9033@gmail.com>
2020-12-16 21:34:15 +0900

    20201216_21:33

--- Python/Webscroller/so.py
+++ Python/Webscroller/so.py
@@ -10,9 +10,22 @@ def get_last_page():
     result = requests.get(URL)
     soup = BeautifulSoup(result.text, "html.parser")
     pages = soup.find("div", {"class": "s-pagination"}).find_all("a")
-    print(pages)
+    last_page = pages[-2].get_text(strip=True)
+    return int(last_page)
+
+
+def extract_jobs(last_page):
+    # list 는 for 밖에 만든다
+    jobs = []
+    for page in range(last_page):
+        result = requests.get(f"{URL}&pg={page+1}")
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.find_all("div", {"class": "-job"})
+        for result in results:
+            print(result["data-jobid"])
 
 
 def get_jobs():
     last_page = get_last_page()
-    return []
+    jobs = extract_jobs(last_page)
+    return jobs
