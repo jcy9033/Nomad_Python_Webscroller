bcf1666c32a1ffb64f29759f5cbe3b9c910e3aa8
jcy9033 <jcy9033@gmail.com>
2020-12-18 15:03:39 +0900

    202018_15:03

---
+++ Python/Flaskscrapper/main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask("Test")
+
+
+@app.route("/")
+def home():
+    return render_template("potato.html")
+
+
+@app.route("/report")
+def report():
+    word = request.args.get('word')
+    return render_template("report.html", searchingBy=word)
+
+app.run(host="0.0.0.0")
